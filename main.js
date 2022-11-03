368012e413708af536b216225c830519a5db965d
sebfrench <sebfrench@outlook.com>
2022-11-03 16:28:04 +0000

    test done

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
+import {jest} from '@jest/globals'
+
 /* 
 👉 Write your kata here!
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function result(score) {
+    let myString = score;
+    let result = {};
+    for (let str of myString) {
+      result[str] = result.hasOwnProperty(str) ? result[str] + 1 : 1;
+    }
+    console.log(result)
+
+    // for (const [key, value] of Object.entries(result)) {
+    //     console.log(`${key}`);
+    // }
+    let arr = Object.values(result);
+    // let letter = Object.keys(result)
+    let max = Math.max(...arr);
+    //search through object
+    console.log(Object.keys(result).filter(function(x){ return result[x] == max; })[0]);
+
+    return (Object.keys(result).filter(function(x){ return result[x] == max; })[0]);
+    //find highest number
+    // //! Object.entries
+    //find letter with highest value
+    //return letter ONLY
+
+}
+
+// function mostOfSameLetter(str) {
+//     let myString = str;
+//     let result = {};
+//     for (let str of myString) {
+//       result[str] = result.hasOwnProperty(str) ? result[str] + 1 : 1;
+//     }
+//     let max = 0;
+//     let maxChar = '';
+//     for (let char in result) {
+//       if (result[char] > max) {
+//         max = result[char];
+//         maxChar = char;
+//       }
+//     }
+//     return maxChar;
+//   }
--- main.test.js
+++ main.test.js
@@ -1,1 +1,22 @@
 //👉 Write your tests below here:
+
+
+//!football game//
+// Team vs Team
+// string as input with goals 
+// team with most goals returns win 
+// if same goals its a draw
+// offside is lowercase
+//e.g liv vs man (LmMLlLM) = 3xliv goals 2xman goals 2x offside
+import {jest} from '@jest/globals'
+import { result } from "./main.js";
+
+test.each([
+    ["LmMLlLM", "L"],
+    ["AaIiI", "I"]
+])("match score", function(score, winner){
+    const actual = result(score);
+    expect(actual).toBe(winner);
+})
+
+
